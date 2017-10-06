a2cd5af8d74985feaea9e633709988f376fac23e
Candidate PayByPhone <pbphire@georgekronberg.com>
2017-10-06 18:07:46 +0000

    Interview Exercise

--- StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -10,5 +10,17 @@ namespace StringCalculator.Tests
     [TestFixture]
     public class StringCalculatorTests
     {
+        [TestCase("",0)]
+        [TestCase("1",1)]
+        [TestCase("1,2",3)]
+        [TestCase("1,2,3",6)]
+        [TestCase("1\n2,3",6)]
+        public void Test(string value, int result)
+        {
+            StringCalculator calculator = new StringCalculator();
+            int test_result = calculator.Add(value);
+
+            Assert.AreEqual(test_result,result);
+        }
     }
 }
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -8,5 +8,43 @@ namespace StringCalculator
 {
     public class StringCalculator
     {
+        private const char comma_delimeter = ',';
+        private const char line_delimeter = '\n';
+        public int Add(string numbers)
+        {
+            //Test empty
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            //Check if string has only 1 number
+            if (numbers.IndexOf(comma_delimeter) == -1 && !numbers.Contains(line_delimeter))
+            {
+                return HandleSingleNumber(numbers);
+            }
+
+            return HandleMultipleNumbers(numbers);
+
+
+        }
+
+        private int HandleSingleNumber(string number)
+        {
+            return Int32.Parse(number);
+        }
+
+        private int HandleMultipleNumbers(string numbers)
+        {
+            var split_numbers = numbers.Split(comma_delimeter, line_delimeter);
+            int sum = 0;
+
+            foreach (var number in split_numbers)
+            {
+                sum += Int32.Parse(number);
+            }
+
+            return sum;
+        }
     }
 }
